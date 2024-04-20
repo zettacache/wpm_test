b4e5edcdba1d14bd92d31cafa7716f84dfeca327
Justin B <zcache@pm.me>
2024-04-19 20:14:50 -0400

    Change import `typing_manager` -> `prompt_manager`

--- zetype/__init__.py
+++ zetype/__init__.py
@@ -1,5 +1,5 @@
 from .calculations import minutes_since, get_accuracy, get_words_typed
-from .typing_manager import TypingManager
+from .prompt_manager import PromptManager
 from .colors import Colors
 from .key import Key
 from .app import App
