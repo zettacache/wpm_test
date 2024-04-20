190d84afe4309338077151deef3af6d15aea28be
Justin B <zcache@pm.me>
2024-04-19 22:42:04 -0400

    Add `input_handler.py` Remove `key.py`

--- zetype/__init__.py
+++ zetype/__init__.py
@@ -1,5 +1,5 @@
 from .calculations import minutes_since, get_accuracy, get_words_typed
-from .prompt_manager import PromptManager
+from .prompt_manager import PromptManager, InputError
+from .input_handler import InputHandler, InputAction
 from .colors import Colors
-from .key import Key
 from .app import App
